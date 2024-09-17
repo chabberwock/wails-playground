ca12987f362da3b3254fb44fec70b4174518c6b6
ale.makarov <ale.makarov@bi.zone>
2024-09-17 12:46:59 +0300

    turn off wails

--- main.go
+++ main.go
@@ -6,11 +6,6 @@ import (
 	"fyne.io/systray/example/icon"
 	"log"
 	"net/http"
-	"wails-playground/internal/myapp"
-
-	"github.com/wailsapp/wails/v2"
-	"github.com/wailsapp/wails/v2/pkg/options"
-	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
 
 //go:embed all:frontend/dist
@@ -28,27 +23,27 @@ func main() {
 	go systray.Run(onReady, onExit)
 
 	// Create an instance of the app structure
-	app := myapp.NewApp()
-
-	// Create application with options
-	err := wails.Run(&options.App{
-		Title:  "wails-playground",
-		Width:  1024,
-		Height: 768,
-		AssetServer: &assetserver.Options{
-			Assets:  assets,
-			Handler: new(MyLoader),
-		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.Startup,
-		Bind: []interface{}{
-			app,
-		},
-	})
-
-	if err != nil {
-		println("Error:", err.Error())
-	}
+	//app := myapp.NewApp()
+	//
+	//// Create application with options
+	//err := wails.Run(&options.App{
+	//	Title:  "wails-playground",
+	//	Width:  1024,
+	//	Height: 768,
+	//	AssetServer: &assetserver.Options{
+	//		Assets:  assets,
+	//		Handler: new(MyLoader),
+	//	},
+	//	BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+	//	OnStartup:        app.Startup,
+	//	Bind: []interface{}{
+	//		app,
+	//	},
+	//})
+	//
+	//if err != nil {
+	//	println("Error:", err.Error())
+	//}
 
 }
 
