250e8dc8d57be436885d21adb95bc9f21e0cd3bf
ale.makarov <ale.makarov@bi.zone>
2024-09-18 10:19:37 +0300

    disable menu item

--- main.go
+++ main.go
@@ -60,9 +60,10 @@ func onReady() {
 	systray.SetIcon(icon.Data)
 	systray.SetTitle("Awesome App")
 	systray.SetTooltip("Pretty awesome")
-	mQuit := systray.AddMenuItem("Quit", "Quit the whole app")
+	//mQuit := systray.AddMenuItem("Quit", "Quit the whole app")
+
 	systray.AddSeparator()
 	systray.AddMenuItemCheckbox("test", "tooltip her", true)
 	// Sets the icon of a menu item. Only available on Mac and Windows.
-	mQuit.SetIcon(icon.Data)
+	//mQuit.SetIcon(icon.Data)
 }
