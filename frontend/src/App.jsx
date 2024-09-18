724a8222be5e9dc7b7e3b73719b48b078d0a8240
Alikhan Gatsalov <a.gatsalov@bi.zone>
2024-09-18 21:42:32 +0300

    try to load module

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,50 @@
-import {useState} from 'react';
+import { useState, useEffect } from 'react';
 import logo from './assets/images/logo-universal.png';
 import './App.css';
-import {Greet} from "../wailsjs/go/myapp/App";
+import { Greet } from '../wailsjs/go/myapp/App';
 
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e) => setName(e.target.value);
-    const updateResultText = (result) => setResultText(result);
+  const [resultText, setResultText] = useState(
+    'Please enter your name below 👇'
+  );
+  const [name, setName] = useState('');
+  const updateName = (e) => setName(e.target.value);
+  const updateResultText = (result) => setResultText(result);
+  const [Remote, setRemote] = useState(null);
 
-    function greet() {
-        Greet(name).then(updateResultText);
-    }
+  function greet() {
+    Greet(name).then(updateResultText);
+  }
+  useEffect(() => {
+    // В режиме dev вот так ../../modules/dist1/main работает, компонент из папки подгружается,
+    // а в сбилденном виде все падает. Я так и не разобрался как правильно прописать путь.
+    import('../../modules/module1/main').then((module) => {
+      setRemote(() => window.default);
+    });
+  }, []);
 
-    return (
-        <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
-            </div>
-        </div>
-    )
+  return (
+    <div id="App">
+      <img src={logo} id="logo" alt="logo" />
+      <div id="result" className="result">
+        {resultText}
+      </div>
+      <div id="input" className="input-box">
+        <input
+          id="name"
+          className="input"
+          onChange={updateName}
+          autoComplete="off"
+          name="input"
+          type="text"
+        />
+        <button className="btn" onClick={greet}>
+          Greet
+        </button>
+        {Remote ? <Remote /> : <div>Loading...</div>}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
